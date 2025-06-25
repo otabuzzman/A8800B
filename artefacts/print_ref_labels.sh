3c3a484a8f92ee8e4f4eb6adcde1e9db1cb7cf88
otabuzzman <iuergen.schuck@gmail.com>
2025-06-25 12:06:45 +0200

    Save work on steps 3 and 4

--- artefacts/print_ref_labels.sh
+++ artefacts/print_ref_labels.sh
@@ -1,12 +1,41 @@
 #!/usr/bin/awk -f
 
+BEGIN {
+  comment = ""
+}
+
+$2 ~ /COMMENT/ {
+  comment = $3
+}
+
+NF > 1 && comment == $2 {
+  comment = ""
+}
+
+comment != "" {
+  next
+}
+
+$2 ~ /;/ { next }
+
 {
     for (nf = 2; nf <= NF ; nf++) {
        if ($nf ~ /^[A-Z0-9]+:$/) {
-            print $nf > "/dev/stderr" ; next
+            print $nf > "/dev/stderr" ; continue
         }
         if ($nf ~ /^(ACI|ADC|ADD|ADI|ANA|ANI|CALL|CC|CM|CMA|CMC|CMP|CNC|CNZ|CP|CPE|CPI|CPO|CZ|DAA|DAD|DCR|DCX|DI|EI|HLT|IN|INR|INX|JC|JM|JMP|JNC|JNZ|JP|JPE|JPO|JZ|LDA|LDAX|LHLD|LXI|MOV|MVI|NOP|ORA|ORI|OUT|PCHL|POP|PUSH|RAL|RAR|RC|RET|RIM|RLC|RM|RNC|RNZ|RP|RPE|RPO|RRC|RST|RZ|SBB|SBI|SHLD|SIM|SPHL|STA|STAX|STC|SUB|SUI|XCHG|XRA|XRI|XTHL)$/) {
-            if ($(nf + 1) ~ /,/) next
+            if ($(nf + 1) ~ /^.,.$/) next
+            if ($(nf + 1) ~ /,/) {
+                split($(nf + 1), t, /,/)
+                if (t[1] ~ /^....+$/) {
+                    print t[1] ; next
+                }
+                if (t[2] ~ /^....+$/) {
+                    print t[2] ; next
+                }
+            }
+            if ($(nf + 1) ~ /;/) next
+            if ($(nf + 1) ~ /"/) next
             if ($(nf + 1) ~ /^.{1,2}$/) next
             print $(nf + 1) ; next
         }
